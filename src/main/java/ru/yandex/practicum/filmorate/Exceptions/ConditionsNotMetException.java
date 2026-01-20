4f3bb62bfddc9f00074b980e1fdf53b37700efdd
Andrei <dobr0804@mail.ru>
2026-01-20 14:40:57 +0300

    1 Правка кодстайла.

--- src/main/java/ru/yandex/practicum/filmorate/Exceptions/ConditionsNotMetException.java
+++ src/main/java/ru/yandex/practicum/filmorate/Exceptions/ConditionsNotMetException.java
@@ -5,6 +5,7 @@ import lombok.Getter;
 @Getter
 public class ConditionsNotMetException extends RuntimeException {
     private final String type;
+
     public ConditionsNotMetException(String message, String type) {
         super(message);
         this.type = type;
