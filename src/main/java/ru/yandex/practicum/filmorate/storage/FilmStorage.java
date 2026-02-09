dabb50a3ad8f4d564da7950ec47e904a0a967b82
Emptybottlecup <dobr0804@mail.ru>
2026-02-09 16:03:17 +0300

    Поправил по кодстайлу - 7.

--- src/main/java/ru/yandex/practicum/filmorate/storage/FilmStorage.java
+++ src/main/java/ru/yandex/practicum/filmorate/storage/FilmStorage.java
@@ -2,7 +2,6 @@ package ru.yandex.practicum.filmorate.storage;
 
 import ru.yandex.practicum.filmorate.model.films.Film;
 
-import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
